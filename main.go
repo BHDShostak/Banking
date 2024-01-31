3684aadb7739bf288512b9765542ac9b86077f76
BHDShostak <BHDShostak@github.com>
2024-01-31 13:42:08 +0100

    firtCommit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+)
+
+type Customer struct {
+	Name    string `json:"full_name" xml:"name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zip_code" xml:"zipcode"`
+}
+
+func main() {
+
+	//define routers
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	//starting server
+	http.ListenAndServe("localhost:8000", nil)
+
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hellow World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Bohdan", "Warszawa", "02097"},
+		{"Marian", "Kyiv", "04235"},
+	}
+
+	if r.Header.Get("Content-Type") == "application/xml" {
+		w.Header().Add("Content-Type", "application/xml")
+		err := xml.NewEncoder(w).Encode(customers)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	} else {
+		w.Header().Add("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(customers)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+
+}
