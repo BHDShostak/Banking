ba147725d3478994df00071ec473709f2cabc37e
BHDShostak <BHDShostak@github.com>
2024-02-29 21:35:46 +0100

    InsertAccToDbWithStatus201

--- app/app.go
+++ app/app.go
@@ -3,10 +3,8 @@ package app
 import (
 	"banking/domain"
 	"banking/service"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -14,15 +12,15 @@ import (
 )
 
 // CMD Application Configuration Options
-func sanityCheck() {
-	if os.Getenv("SERVER_ADDRESS") == "" || os.Getenv("SERVER_PORT") == "" {
-		log.Fatal("SERVER_ADDRESS and SERVER_PORT must be set")
-	}
-}
+// func sanityCheck() {
+// 	if os.Getenv("SERVER_ADDRESS") == "" || os.Getenv("SERVER_PORT") == "" {
+// 		log.Fatal("SERVER_ADDRESS and SERVER_PORT must be set")
+// 	}
+// }
 
 func Start() {
 
-	sanityCheck()
+	// sanityCheck()
 
 	router := mux.NewRouter()
 
@@ -30,25 +28,27 @@ func Start() {
 	//ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
 	dbClient := getDbClient()
 	customerRepositoryDB := domain.NewCustomerRepositoryDB(dbClient)
-	//accountRepositoryDB := domain.NewAccountRepositoryDB(dbClient)
+	accountRepositoryDB := domain.NewAccountRepositoryDB(dbClient)
 	ch := CustomerHandlers{service.NewCustomerService(customerRepositoryDB)}
+	ah := AccountHandler{service.NewAccountService(accountRepositoryDB)}
 	//define routers
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).Methods(http.MethodPost)
 
 	//starting server
-	address := os.Getenv("SERVER_ADDRESS")
-	port := os.Getenv("SERVER_POST")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router)) // CMD Application Configuration Options
-	//log.Fatal(http.ListenAndServe("localhost:8000", router)) // Standard Application Configuration Options
+	// address := os.Getenv("SERVER_ADDRESS")
+	// port := os.Getenv("SERVER_POST")
+	// log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router)) // CMD Application Configuration Options
+	log.Fatal(http.ListenAndServe("localhost:8000", router)) // Standard Application Configuration Options
 }
 
 func getDbClient() *sqlx.DB {
-	dbUser := os.Getenv("DB_USER")
-	dbPasswd := os.Getenv("DB_PASSWD")
-	dbAddr := os.Getenv("DB_ADDR")
-	dbPort := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	// dbUser := os.Getenv("DB_USER")
+	// dbPasswd := os.Getenv("DB_PASSWD")
+	// dbAddr := os.Getenv("DB_ADDR")
+	// dbPort := os.Getenv("DB_PORT")
+	// dbName := os.Getenv("DB_NAME")
 
 	// root:my-secret-pw@tcp(localhost:3306)/banking // old way
 	// export DB_USER=root
@@ -56,10 +56,11 @@ func getDbClient() *sqlx.DB {
 	// export DB_ADDR=localhost
 	// export DB_PORT=3306
 	// export DB_NAME=banking
+	// SERVER_ADDRESS=localhost SERVER_PORT=8000 DB_USER=root DB_PASSWD=my-secret-pw DB_ADDR=localhost DB_PORT=3306 DB_NAME=banking
 
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
+	// dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
 
-	client, err := sqlx.Open("mysql", dataSource)
+	client, err := sqlx.Open("mysql", "root:my-secret-pw@tcp(localhost:3306)/banking")
 	if err != nil {
 		panic(err)
 	}
--- service/accountService.go
+++ service/accountService.go
@@ -16,6 +16,10 @@ type DefaultAccountService struct {
 }
 
 func (s DefaultAccountService) NewAccount(req dto.NewAccountRequest) (*dto.NewAccountResponse, *errs.AppError) {
+	err := req.Validate()
+	if err != nil {
+		return nil, err
+	}
 	a := domain.Account{
 		AccountId:   "",
 		CustomerId:  req.CustomerId,
